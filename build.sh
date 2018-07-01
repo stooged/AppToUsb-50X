153f0f695b2c5b498eeb60357bb5afaad9d6a9aa
stooged <stooged@users.noreply.github.com>
2018-07-01 12:50:20 +0800

    v1

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ rm -f bin/AppToUsb.bin
 cp AppToUsb/AppToUsb.bin bin/AppToUsb.bin
 mkdir -p html_payload
 tool/bin2js bin/AppToUsb.bin > html_payload/payload.js
-sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/AppToUsb.html
+FILESIZE=$(stat -c%s "bin/AppToUsb.bin")
+PNAME=$"AppToUsb"
+cp exploit.template html_payload/AppToUsb.html
+sed -i -f - html_payload/AppToUsb.html << EOF
+s/#NAME#/$PNAME/g
+s/#BUF#/$FILESIZE/g
+s/#PAY#/$(cat html_payload/payload.js)/g
+EOF
 rm -f AppToUsb/AppToUsb.bin
 rm -f html_payload/payload.js
